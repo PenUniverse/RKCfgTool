3701c6080a312fe00d0d3d00c06656ab6a0e0481
Redbeanw44602 <redbeana44945@gmail.com>
2024-09-09 23:10:24 +0800

    chore: add project base files.

---
+++ xmake.lua
@@ -0,0 +1,17 @@
+add_rules('mode.debug', 'mode.release')
+
+add_requires('argparse      3.1')
+add_requires('spdlog        1.14.1')
+add_requires('icu4c         75.1')
+add_requires('nlohmann_json 3.11.3')
+
+target('para2cfg')
+    set_kind('binary')
+    add_files('src/**.cpp')
+    add_includedirs('src')
+    set_warnings('all')
+    set_languages('c99', 'c++23')
+    add_packages('argparse', 'spdlog', 'icu4c', 'nlohmann_json')
+    if is_mode('debug') then 
+        add_defines('DEBUG')
+    end
